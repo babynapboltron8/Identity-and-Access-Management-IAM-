68a495a7d820c20d50df8d6abf97fa7c7c092899
Baby Nap Boltron <babynap8@gmail.com>
2026-07-26 16:43:44 +0800

    update user entities

--- Entities/User.cs
+++ Entities/User.cs
@@ -4,7 +4,6 @@ public class User
 {
     public int Id { get; set; }
     public string Username { get; set; } = null!;
-    public string Password { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string PasswordHash { get; set; } = null!;
     public bool IsActive { get; set; }
