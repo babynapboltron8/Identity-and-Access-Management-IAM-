62a99f5065e24693112fb99c1b4c47ef2de14ee0
Baby Nap Boltron <babynap8@gmail.com>
2026-07-26 17:34:11 +0800

    add permission entities code properties

--- Entities/Permission.cs
+++ Entities/Permission.cs
@@ -5,5 +5,6 @@ namespace IAM_API.Entities
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
     }
 }
