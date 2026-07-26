f151ae2f60a53778a8ab290fa9c7f199bee1e9fb
Baby Nap Boltron <babynap8@gmail.com>
2026-07-26 17:41:37 +0800

    update refreshtoken properties

--- Entities/RefreshToken.cs
+++ Entities/RefreshToken.cs
@@ -6,6 +6,7 @@ namespace IAM_API.Entities
         public int UserId { get; set; }
         public string Token { get; set; } = string.Empty;
         public DateTime ExpiresAt { get; set; }
-        public bool Revoked { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
