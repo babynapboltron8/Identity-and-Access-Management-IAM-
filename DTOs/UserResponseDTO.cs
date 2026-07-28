5c0e57c9f54768fc206196a14daf41a924d36940
Baby Nap Boltron <babynap8@gmail.com>
2026-07-28 22:03:49 +0800

    organize code user response dto

--- DTOs/UserResponseDTO.cs
+++ DTOs/UserResponseDTO.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 namespace IAM_API.DTOs
 {
     public class UserResponseDto
-{
-    public Guid Id { get; set; }
-    public string Username { get; set; } = "";
-    public string Email { get; set; } = "";
-    public bool IsActive { get; set; }
-    public bool IsLocked { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; } = "";
+        public string Email { get; set; } = "";
+        public bool IsActive { get; set; }
+        public bool IsLocked { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
 }
